9cfe2211850aa944ae26fefaacb9f75bc6257b6e
jahid1971 <jahidhasank1971@gmail.com>
2024-02-11 11:21:19 +0100

    why us created

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,7 +1,7 @@
 import HeroSection from "@/components/ui/homepage/heroSection/HeroSection";
 import Specialist from "@/components/ui/homepage/specialist/Specialist";
 import TopRatedDoctors from "@/components/ui/homepage/topRatedDoctors/TopRatedDoctors";
-
+import WhyUs from "@/components/ui/homepage/whyUs/WhyUs";
 
 const HomePage = () => {
     return (
@@ -9,7 +9,7 @@ const HomePage = () => {
             <HeroSection />
             <Specialist />
             <TopRatedDoctors />
-       
+            <WhyUs />
         </>
     );
 };
---
+++ src/components/ui/homepage/whyUs/WhyUs.tsx
@@ -0,0 +1,79 @@
+import assets from "@/assets";
+import chooseUsImg from "@/assets/choose-us.png";
+import { Box, Container, Grid, Stack, Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import Image from "next/image";
+
+const servicesData = [
+    {
+        imageSrc: assets.svgs.award,
+        title: "Award Winning Service",
+        description:
+            "Recognized for excellence in patient care, ensuring the highest standards in all aspects of our services.",
+    },
+    {
+        imageSrc: assets.svgs.care,
+        title: "Best Quality Pregnancy Care",
+        description:
+            "Providing comprehensive and compassionate care to expectant mothers for a healthy pregnancy and safe delivery.",
+    },
+    {
+        imageSrc: assets.svgs.equipment,
+        title: "Complete Medical Equipments",
+        description:
+            "Equipped with the latest medical technology to ensure accurate diagnosis and effective treatment for all patients.",
+    },
+    {
+        imageSrc: assets.svgs.call,
+        title: "Dedicated Emergency Care",
+        description:
+            "Offering 24/7 emergency services with a team of experienced professionals ready to handle any medical crisis.",
+    },
+];
+
+const WhyUs = () => {
+    return (
+        <Container>
+            <Box>
+                <Box textAlign={"center"}>
+                    <Typography variant="h6" color={"primary"} fontWeight={700}>
+                        Why Us
+                    </Typography>
+                    <Typography variant="h4" fontWeight={700}>
+                        Why Choose Us
+                    </Typography>
+                </Box>
+            </Box>
+            <Grid container direction={{ md: "row" }} spacing={5} alignItems={"center"} py={2}>
+                <Grid item md={6}>
+                    {servicesData.map((service, index) => (
+                        <Box
+                            key={index}
+                            display={"flex"}
+                            alignItems={"center"}
+                            justifyContent={"center"}
+                            gap={2}
+                            sx={{ backgroundColor: grey[100], px: 4, py: 3, my: 3 }}
+                            borderRadius={index % 2 === 0 ? "10px 10px 100px 10px" : "10px 100px 10px 10px"}>
+                            <Box>
+                                <Image src={service.imageSrc} alt={service.title} width={50} />
+                            </Box>
+                            <Box>
+                                <Typography variant="h6" fontWeight={700}>
+                                    {service.title}
+                                </Typography>
+                                <Typography>{service.description}</Typography>
+                            </Box>
+                        </Box>
+                    ))}
+                </Grid>
+
+                <Grid item md={6}>
+                    <Image src={chooseUsImg} alt="Why Us" width={480} />
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default WhyUs;
